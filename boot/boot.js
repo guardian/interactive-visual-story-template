cc21c933efa75795c122c1fb36298ae1c98b6e1e
feilding <feilding.cage@gmail.com>
2015-07-24 16:30:26 +0100

    a few andriod app updates

--- boot/boot.js
+++ boot/boot.js
@@ -1,4 +1,10 @@
 define(['/js/main.js'], function(app) {
+
+	var els = document.querySelectorAll('.article__body,.element-interactive');
+ 	for(var e = 0; e < els.length; e ++){
+ 		els[e].style.minHeight = '100vh';
+ 	}
+
     var css = document.createElement('link');
     css.type = 'text/css';
     css.rel = 'stylesheet';
